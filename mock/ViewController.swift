f31b0352dc4556ffa195e358d6a3841012c945ca
mzgk <mzgkworks@gmail.com>
2018-02-28 16:14:21 +0900

    Initial Commit

---
+++ mock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mock
+//
+//  Created by mzgk on 2018/02/28.
+//  Copyright © 2018年 mzgk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
